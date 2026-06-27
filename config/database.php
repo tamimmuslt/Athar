9630a025fab71c4af6c6bd32755503dc1a42caa3
Your Name <you@example.com>
2026-06-27 13:43:27 +0300

    Fix: Enable SSL options for MySQL connection

--- config/database.php
+++ config/database.php
@@ -62,9 +62,8 @@ return [
         //    'options' => extension_loaded('pdo_mysql') ? array_filter([
         //         (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
         //     ]) : [],
-      'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => true, // هذا السطر المهم لتفعيل قبول اتصال SSL المجبرين عليه
-    ]) : [],
+     'options' => [
+    'PDO::MYSQL_ATTR_SSL_CA' => true,
 ],
         ],
 
