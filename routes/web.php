dcbb0f13cb7c6171af0eb586488f0902e70bd570
Your Name <you@example.com>
2026-06-27 13:22:06 +0300

    Add temporary route to run migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('/run-migration', function () {
+    try {
+        Artisan::call('migrate --force');
+        return 'قاعدة البيانات جاهزة والجداول رُفعت بنجاح! 🎉<br><pre>' . Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return 'حدث خطأ أثناء رفع الجداول: ' . $e->getMessage();
+    }
+});
 Route::get('/', function () {
     return view('welcome');
 });
